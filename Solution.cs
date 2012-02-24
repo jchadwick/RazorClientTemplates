9f5f0f563782d702ca42bf97f62e5e952fac07e4
Jess Chadwick <jesschadwick@gmail.com>
2012-02-24 02:01:31 -0500

    Creating NuGet package

---
+++ Solution.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("Jess Chadwick")]
+[assembly: AssemblyCopyright("Copyright Jess Chadwick ©  2012")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
--- Website/Properties/AssemblyInfo.cs
+++ Website/Properties/AssemblyInfo.cs
@@ -1,35 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Website")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Website")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ae3e224b-3ff5-4146-a0e5-737d06d5e7a3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTitle("Razor Client Templates -- Demo Website")]
+[assembly: AssemblyProduct("Razor Client Templates -- Demo Website")]
+[assembly: AssemblyDescription("Demo website for the Razor Client Templating library")]
