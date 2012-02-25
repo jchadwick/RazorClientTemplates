9dee8629a06f3b655660c49d702c1707e7d06876
Jess Chadwick <jesschadwick@gmail.com>
2012-02-25 01:00:37 -0500

    NuGet tweaks

--- Solution.cs
+++ Solution.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Jess Chadwick")]
 [assembly: AssemblyCopyright("Copyright Jess Chadwick ©  2012")]
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
